aa84591f61e9229bce51af65176f65b0d5cdf1d4
Mingxun Wang <mwang87@gmail.com>
2019-02-26 11:09:23 -0800

    wrapping mscluster

---
+++ metabolomics-snets-v2/tools/metabolomicsnetsv2/scripts/mscluster_wrapper.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import sys
+import os
+
+def usage():
+    print("<input spectra> <parameters filename> <output spectra> <output aligns> <main specnets filepath>")
+
+def main():
+    usage()
+
+    input_spectra_folder = sys.argv[1]
+    parameters_filename = sys.argv[2]
+    output_spectra_folder = sys.argv[3]
+    output_alignments_folder = sys.argv[4]
+    mainspecnets_binary = sys.argv[5]
+
+    cmd = "%s %s -ll 9 -f mscluster" % (mainspecnets_binary, parameters_filename)
+    ret_code = os.system(cmd)
+
+    if ret_code != 0:
+        exit(1)
+
+    #Do clean up out output spectra folder
+    
+
+
+
+if __name__ == "__main__":
+    main()
