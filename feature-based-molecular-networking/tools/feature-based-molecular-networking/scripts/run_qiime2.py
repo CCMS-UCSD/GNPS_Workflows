3730b751c5324d8b5ac5df29785d19c078473cb7
Mingxun Wang <mwang87@gmail.com>
2019-03-05 19:31:38 -0800

    updating to support no metadata

--- feature-based-molecular-networking/tools/feature-based-molecular-networking/scripts/run_qiime2.py
+++ feature-based-molecular-networking/tools/feature-based-molecular-networking/scripts/run_qiime2.py
@@ -4,8 +4,6 @@ import sys
 import requests
 import shutil
 
-"""TODO: Handle no metadata file input"""
-
 input_metadata_filename = sys.argv[1]
 input_quantification_table = sys.argv[2]
 output_folder = sys.argv[3]
@@ -15,48 +13,41 @@ output_manifest_filename = os.path.join(output_folder, "qiime2_manifest.tsv")
 
 df_quantification = pd.read_csv(input_quantification_table, sep=",")
 
+"""Reading Metadata Filename and filling in empty entries"""
 if len(input_metadata_filename) > 2:
     df_metadata = pd.read_csv(input_metadata_filename, sep="\t")
-    if "sample_name" in df_metadata:
-        manifest_df = df_metadata["sample_name", "filename"]
-    else:
-        manifest_df = pd.DataFrame()
-        manifest_df["sample_name"] = df_metadata["filename"]
-        manifest_df["filepath"] = df_metadata["filename"]
 else:
     df_metadata = pd.DataFrame([{"filename": "placeholder"}])
-    manifest_df = pd.DataFrame()
-    manifest_df["sample_name"] = df_metadata["filename"]
-    manifest_df["filepath"] = df_metadata["filename"]
 
+if not "sample_name" in df_metadata:
+    df_metadata["sample_name"] = df_metadata["filename"]
 
 """Checking if the set of filenames are fully covered, if not then we'll provide a place holder"""
 all_quantification_filenames = [key.split(" ")[0] for key in df_quantification.keys() if "Peak area" in key]
+metadata_filenames = list(df_metadata["filename"])
 
-manifest_filenames = list(manifest_df["filepath"])
-
-manifest_object_list = manifest_df.to_dict(orient="records")
+metadata_object_list = df_metadata.to_dict(orient="records")
 for quantification_filename in all_quantification_filenames:
-    if not quantification_filename in manifest_filenames:
+    if not quantification_filename in metadata_filenames:
         print(quantification_filename, "not found")
-        manifest_object = {}
-        manifest_object["filepath"] = quantification_filename
-        manifest_object["sample_name"] = quantification_filename
-        manifest_object_list.append(manifest_object)
+        metadata_object = {}
+        metadata_object["filename"] = quantification_filename
+        metadata_object["sample_name"] = quantification_filename
+        metadata_object_list.append(metadata_object)
 
-pd.DataFrame(manifest_object_list).to_csv(output_manifest_filename, index=False, sep=",")
-
-"""Reformatting the metadata filename"""
-if "sample_name" in df_metadata:
-    new_output_metadata = df_metadata
-else:
-    new_output_metadata = df_metadata
-    new_output_metadata["sample_name"] = df_metadata["filename"]
+"""Adding in missing filenames into the metadata"""
+new_output_metadata = pd.DataFrame(metadata_object_list)
 
 output_columns = list(new_output_metadata.keys())
 output_columns.remove("sample_name")
 output_columns.insert(0, "sample_name")
 new_output_metadata.to_csv(output_metadata_filename, index=False, sep="\t", columns=output_columns)
+
+"""Outputting Manifest Filename"""
+manifest_df = pd.DataFrame()
+manifest_df["sample_name"] = new_output_metadata["sample_name"]
+manifest_df["filepath"] = new_output_metadata["filename"]
+manifest_df.to_csv(output_manifest_filename, index=False, sep=",")
 
 """Calling remote server to do the calculation"""
 SERVER_BASE = "http://dorresteinappshub.ucsd.edu:5024"
