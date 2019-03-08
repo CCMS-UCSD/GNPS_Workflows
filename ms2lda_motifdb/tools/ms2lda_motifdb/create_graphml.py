c901feeb39a2202fa9bf4e98aeaccf1621f7720a
Mingxun Wang <mwang87@gmail.com>
2019-03-07 18:01:20 -0800

    work in progress

--- ms2lda_motifdb/tools/ms2lda_motifdb/create_graphml.py
+++ ms2lda_motifdb/tools/ms2lda_motifdb/create_graphml.py
@@ -1,8 +1,14 @@
 import os
 import sys
+from pyMolNetEnhancer import *
 
 input_folder = sys.argv[1]
 output_folder = sys.argv[2]
+
+edges = pd.read_csv(os.path.join(input_folder, "")), sep = '\t')
+
+motif_graph = make_motif_graphml(motif_network['nodes'],motif_network['edges'])
+
 
 output_graphml_filename = os.path.join(output_folder, "ms2lda_network.graphml")
 
