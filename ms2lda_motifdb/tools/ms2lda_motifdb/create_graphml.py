b1504bb83ef4e9fa424196313ebfc463ccd4a9a7
Mingxun Wang <mwang87@gmail.com>
2019-03-07 18:37:32 -0800

    deploying updated code

--- ms2lda_motifdb/tools/ms2lda_motifdb/create_graphml.py
+++ ms2lda_motifdb/tools/ms2lda_motifdb/create_graphml.py
@@ -1,16 +1,15 @@
 import os
 import sys
 from pyMolNetEnhancer import *
+import pandas as pd
 
 input_folder = sys.argv[1]
 output_folder = sys.argv[2]
 
-edges = pd.read_csv(os.path.join(input_folder, "")), sep = '\t')
+edges = pd.read_csv(os.path.join(input_folder, "output_ms2lda_edges.tsv"), sep = '\t')
+nodes = pd.read_csv(os.path.join(input_folder, "output_ms2lda_nodes.tsv"), sep = '\t')
 
-motif_graph = make_motif_graphml(motif_network['nodes'],motif_network['edges'])
-
+motif_graph = make_motif_graphml(nodes, edges)
 
 output_graphml_filename = os.path.join(output_folder, "ms2lda_network.graphml")
-
-with open(output_graphml_filename, "w") as output_file:
-    output_file.write("DONE")
+nx.write_graphml(motif_graph, output_graphml_filename, infer_numeric_types = True)
