2f4e4567d884d58aac4e70b4d1c4a7eeb16e0ce3
Mingxun Wang <mwang87@gmail.com>
2019-07-05 11:31:16 -0700

    bug fix

--- search_single_spectrum/tools/search_single_spectrum/reanalysis_preparation.py
+++ search_single_spectrum/tools/search_single_spectrum/reanalysis_preparation.py
@@ -11,22 +11,26 @@ def main():
     input_files_filename = sys.argv[1]
     output_filename = sys.argv[2]
 
-    filenames_df = pd.read_csv(input_files_filename, sep="\t")
-    all_filenames = list(filenames_df["filename"])
+    try:
+        filenames_df = pd.read_csv(input_files_filename, sep="\t")
+        all_filenames = list(filenames_df["filename"])
 
-    params_dict = {}
-    params_dict["desc"] = "Meta-Analysis on GNPS"
-    params_dict["workflow"] = "METABOLOMICS-SNETS-V2"
-    params_dict["library_on_server"] = "d.speclibs;"
-    params_dict["spec_on_server"] = ";".join(all_filenames)
+        params_dict = {}
+        params_dict["desc"] = "Meta-Analysis on GNPS"
+        params_dict["workflow"] = "METABOLOMICS-SNETS-V2"
+        params_dict["library_on_server"] = "d.speclibs;"
+        params_dict["spec_on_server"] = ";".join(all_filenames)
 
-    print(params_dict)
+        print(params_dict)
 
-    #reanalysis_url = "https://gnps.ucsd.edu/ProteoSAFe/index.jsp?#" + json.dumps(params_dict)
+        #reanalysis_url = "https://gnps.ucsd.edu/ProteoSAFe/index.jsp?#" + json.dumps(params_dict)
 
-    output_file = open(output_filename, "w")
-    output_file.write("filenames\n")
-    output_file.write(";".join(all_filenames))
+        output_file = open(output_filename, "w")
+        output_file.write("filenames\n")
+        output_file.write(";".join(all_filenames))
+    except:
+        output_file = open(output_filename, "w")
+        output_file.close()
 
 if __name__ == "__main__":
     main()
