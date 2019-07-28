5d0f4685bcdc81ba6db3a566238b66e16112a48b
Mingxun Wang <mwang87@gmail.com>
2019-07-28 14:29:34 -0500

    adding reference input files for progenesis

--- feature-based-molecular-networking/test/test_converters.py
+++ feature-based-molecular-networking/test/test_converters.py
@@ -3,6 +3,7 @@ import sys
 sys.path.insert(0, "../tools/feature-based-molecular-networking/scripts/")
 import msdial_formatter
 import progenesis_formatter
+import metaboscape_formatter
 
 class TestLoaders(unittest.TestCase):
 
@@ -13,6 +14,10 @@ class TestLoaders(unittest.TestCase):
     def test_progenesis(self):
         progenesis_formatter.convert_to_feature_csv("./reference_input_file_for_formatter/Progensis_MSE/SONAR_20_Yeast_Peaks.csv", \
             "./progenesis_output.csv")
+
+    def test_metaboscape(self):
+        metaboscape_formatter.convert_to_feature_csv("./reference_input_file_for_formatter/metaboscape/quantification_table-00000.csv", \
+            "./metaboscape_output.csv")
 
 
 if __name__ == '__main__':
