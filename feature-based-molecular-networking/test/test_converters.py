46f433f9545abe5b7b750b8c3b22985fd1dbb206
Mingxun Wang <mwang87@gmail.com>
2019-07-28 14:27:50 -0500

    adding tests

---
+++ feature-based-molecular-networking/test/test_converters.py
@@ -0,0 +1,19 @@
+import unittest
+import sys
+sys.path.insert(0, "../tools/feature-based-molecular-networking/scripts/")
+import msdial_formatter
+import progenesis_formatter
+
+class TestLoaders(unittest.TestCase):
+
+    def test_msdial(self):
+        msdial_formatter.convert_to_feature_csv("./reference_input_file_for_formatter/MS-DIAL/MS-DIAL-GNPS_AG_test_featuretable.txt", \
+            "./msdial_output.csv")
+
+    def test_progenesis(self):
+        progenesis_formatter.convert_to_feature_csv("./reference_input_file_for_formatter/Progensis_MSE/SONAR_20_Yeast_Peaks.csv", \
+            "./progenesis_output.csv")
+
+
+if __name__ == '__main__':
+    unittest.main()
