37f0d10e32cc3398f46461dbf15e7bbbe8be23c7
Mingxun Wang <mwang87@gmail.com>
2019-07-28 14:34:01 -0500

    adding asserting that files are the same after conversion

--- feature-based-molecular-networking/test/test_converters.py
+++ feature-based-molecular-networking/test/test_converters.py
@@ -1,5 +1,6 @@
 import unittest
 import sys
+import filecmp
 sys.path.insert(0, "../tools/feature-based-molecular-networking/scripts/")
 import msdial_formatter
 import progenesis_formatter
@@ -10,6 +11,9 @@ class TestLoaders(unittest.TestCase):
     def test_msdial(self):
         msdial_formatter.convert_to_feature_csv("./reference_input_file_for_formatter/MS-DIAL/MS-DIAL-GNPS_AG_test_featuretable.txt", \
             "./msdial_output.csv")
+
+        self.assertTrue(filecmp.cmp("./msdial_output.csv", "./reference_input_file_for_formatter/MS-DIAL/reference_output_file.csv", shallow=False))
+
 
     def test_progenesis(self):
         progenesis_formatter.convert_to_feature_csv("./reference_input_file_for_formatter/Progensis_MSE/SONAR_20_Yeast_Peaks.csv", \
