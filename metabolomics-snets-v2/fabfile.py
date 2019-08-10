7f9c8ab99cebe23df87568204df5440f4c47908c
Mingxun Wang <mwang87@gmail.com>
2019-08-09 17:08:30 -0700

    initial renaming of the workflow paths

--- metabolomics-snets-v2/fabfile.py
+++ metabolomics-snets-v2/fabfile.py
@@ -6,9 +6,19 @@ env.hosts=['proteomics2.ucsd.edu']
 env.user='miw023'
 #env.gateway="mingxun@mingwangbeta.ucsd.edu"
 
+VERSION="1.2.5"
+
 def update_workflow():
+    #Making workflow the default version
     put(os.path.join(os.getcwd(), 'metabolomics-snets-v2'), "/ccms/workflows/")
-    put(os.path.join(os.getcwd(), 'tools/metabolomicsnetsv2'), "/data/cluster/tools/", mirror_local_mode=True)
+
+    #Uploading workflow to a specific version
+    run("mkdir /ccms/workflows/metabolomics-snets-v2/versions", warn_only=True)
+    run("mkdir /ccms/workflows/metabolomics-snets-v2/versions/%s" % (VERSION) , warn_only=True)
+    run("cp /ccms/workflows/metabolomics-snets-v2/*.xml /ccms/workflows/metabolomics-snets-v2/versions/%s/" % (VERSION) )
+    
+    #Uploading tools to a specific version area
+    #put(os.path.join(os.getcwd(), 'tools/metabolomicsnetsv2'), "/data/cluster/tools/", mirror_local_mode=True)
 
 def update_workflow_gnps():
     local_path = os.path.join(os.getcwd(), 'tools/metabolomicsnetsv2')
