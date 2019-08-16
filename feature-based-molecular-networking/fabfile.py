6afcfbe7d86786b383b848456e70dfd39f29d351
Mingxun Wang <mwang87@gmail.com>
2019-08-16 15:57:20 -0700

    fixing fab deployment

--- feature-based-molecular-networking/fabfile.py
+++ feature-based-molecular-networking/fabfile.py
@@ -1,22 +1,21 @@
 from fabric.api import *
 import os
 
-#env.hosts=['proteomics2.ucsd.edu']
-env.hosts=['gnps.ucsd.edu']
+env.hosts=['proteomics2.ucsd.edu']
+#env.hosts=['gnps.ucsd.edu']
 env.user='miw023'
 
+#VERSION="1.2.3"
+
 def update_workflow():
+    #Making workflow the default version
+    put(os.path.join(os.getcwd(), 'feature-based-molecular-networking'), "/ccms/workflows/")
+
     local_path = os.path.join(os.getcwd(), 'tools/feature-based-molecular-networking')
     temp_path_copy = '/Users/{}/temp_tools'.format(env.user)
     temp_path = '/Users/{}/temp_tools/feature-based-molecular-networking'.format(env.user)
     final_path = '/data/cluster/tools/'
     update_folder(local_path, temp_path_copy, temp_path, final_path, user=env.user)
-
-    local_path = os.path.join(os.getcwd(), 'feature-based-molecular-networking')
-    temp_path_copy = '/Users/{}/temp'.format(env.user)
-    temp_path = '/Users/{}/temp/feature-based-molecular-networking'.format(env.user)
-    final_path = '/ccms/workflows/'
-    update_folder(local_path, temp_path_copy, temp_path, final_path, user="ccms")
 
 def update_workflow_gnps():
     local_path = os.path.join(os.getcwd(), 'tools/feature-based-molecular-networking')
