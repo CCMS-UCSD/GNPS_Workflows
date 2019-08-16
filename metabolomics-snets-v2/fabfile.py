1724bd50ea2a969595eb5d489c49c300cdcab3a4
Mingxun Wang <mwang87@gmail.com>
2019-08-16 11:46:13 -0700

    setting version 1.2.5

--- metabolomics-snets-v2/fabfile.py
+++ metabolomics-snets-v2/fabfile.py
@@ -6,8 +6,7 @@ env.hosts=['gnps.ucsd.edu']
 env.user='miw023'
 #env.gateway="mingxun@mingwangbeta.ucsd.edu"
 
-VERSION="1.2.3"
-#VERSION="1.2.5"
+VERSION="1.2.5"
 #VERSION="release_8"
 
 def update_workflow():
