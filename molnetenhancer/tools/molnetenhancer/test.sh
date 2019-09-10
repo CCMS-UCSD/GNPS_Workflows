0e5ce353dd9ee5f2cbe86cb04ffa4201429639d5
Mingxun Wang <mwang87@gmail.com>
2019-09-10 16:16:39 -0700

    support fbmn

--- molnetenhancer/tools/molnetenhancer/test.sh
+++ molnetenhancer/tools/molnetenhancer/test.sh
@@ -1,9 +1,9 @@
 #FBMN Test
 
-#python ./molnetenhancer.py b817262cb6114e7295fee4f73b22a3ad ./results --nap c4bb6b8be9e14bdebe87c6ef3abe11f6
-#python ./metabodisttree.py b817262cb6114e7295fee4f73b22a3ad ./results ./metabodisttree_results
+#python ./molnetenhancer.py 310e075127dc43dd88a4919bff4f0caf ./results --nap c4bb6b8be9e14bdebe87c6ef3abe11f6
+python ./metabodisttree.py 310e075127dc43dd88a4919bff4f0caf ./results ./metabodisttree_results
 
 #Classical Test
 
 #python ./molnetenhancer.py 30fd8bd601224f8d8a5ca2e65bc4c576 ./classical_results
-python ./metabodisttree.py 30fd8bd601224f8d8a5ca2e65bc4c576 ./classical_results ./classical_metabodisttree_results
+#python ./metabodisttree.py 30fd8bd601224f8d8a5ca2e65bc4c576 ./classical_results ./classical_metabodisttree_results
