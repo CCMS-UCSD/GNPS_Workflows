b7bb2e52415edf354b91948de1662aff0c2a8303
Mingxun Wang <mwang87@gmail.com>
2019-09-16 15:18:20 -0700

    adding citation for mscluster

--- metabolomics-snets-v2/tools/metabolomicsnetsv2/scripts/write_description.py
+++ metabolomics-snets-v2/tools/metabolomicsnetsv2/scripts/write_description.py
@@ -29,5 +29,10 @@ if param_obj["FILTER_LIBRARY"][0] == "1":
 output_sentences.append("All matches kept between network spectra and library spectra were required to have a score above %s and at least %s matched peaks." % (param_obj["SCORE_THRESHOLD"][0], param_obj["MIN_MATCHED_PEAKS_SEARCH"][0]))
 output_sentences.append("<br><br>\n<strong>Citation</strong><br><br>\n")
 output_sentences.append('Wang, Mingxun, et al. "Sharing and community curation of mass spectrometry data with Global Natural Products Social Molecular Networking." Nature Biotechnology 34.8 (2016): 828-837. PMID: 27504778, https://www.nature.com/articles/nbt.3597')
+output_sentences.append('<br><br>')
+
+if param_obj["RUN_MSCLUSTER"] == "on":
+    output_sentences.append('Frank, Ari M., et al. "Clustering millions of tandem mass spectra." Journal of proteome research 7.01 (2007): 113-122. PMID: 18067247, https://pubs.acs.org/doi/abs/10.1021/pr070361e')
+
 
 open(output_filename, "w").write(" ".join(output_sentences))
