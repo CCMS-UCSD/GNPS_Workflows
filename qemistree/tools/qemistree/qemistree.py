7cd22e4c1a530369323034aa441dfb74f5183165
Mingxun Wang <mwang87@gmail.com>
2019-10-04 21:56:29 -0700

    adding the tools to make the tree

--- qemistree/tools/qemistree/qemistree.py
+++ qemistree/tools/qemistree/qemistree.py
@@ -20,6 +20,9 @@ def main():
     output_mgf_qza = os.path.join(args.output_folder, "sirius.mgf.qza")
     output_fragtree_qza = os.path.join(args.output_folder, "fragmentation_trees.qza")
     output_formula_qza = os.path.join(args.output_folder, "formula.qza")
+    output_fingerprints_qza = os.path.join(args.output_folder, "fingerprints.qza")
+    output_merged_feature_table_qza = os.path.join(args.output_folder, "merged_feature_table.qza")
+    output_merged_data_qza = os.path.join(args.output_folder, "merged_data.qza")
 
     all_cmd = []
     cmd = "source {} {} && LC_ALL=en_US && export LC_ALL && qiime tools import --input-path {} --output-path {} --type FeatureTable[Frequency]".format(args.conda_activate_bin, args.conda_environment, args.input_quant_table, output_feature_qza)
@@ -45,12 +48,24 @@ def main():
     --o-molecular-formulas {}'.format(args.conda_activate_bin, args.conda_environment, args.sirius_bin, output_mgf_qza, output_fragtree_qza, output_formula_qza)
     all_cmd.append(cmd)
 
-    # cmd = 'LC_ALL=en_US && export LC_ALL && {} qemistree make-hierarchy \
-    # --i-csi-results {} \
-    # --i-feature-tables {} \
-    # --o-tree demo-qemistree.qza \
-    # --o-merged-feature-table filtered-feature-table.qza \
-    # --o-merged-feature-data feature-data.qza'.format(args.qiime_bin, )
+    cmd = 'source {} {} && LC_ALL=en_US && export LC_ALL && qiime qemistree predict-fingerprints --p-sirius-path {} \
+    --i-molecular-formulas {} \
+    --p-ppm-max 20 \
+    --p-java-flags "-Djava.io.tmpdir=./temp -Xms16G -Xmx64G" \
+    --o-predicted-fingerprints {}'.format(args.conda_activate_bin, args.conda_environment, args.sirius_bin, output_formula_qza, output_fingerprints_qza)
+    all_cmd.append(cmd)
+
+    cmd = 'source {} {} && LC_ALL=en_US && export LC_ALL && qiime qemistree make-hierarchy \
+    --i-csi-results {} \
+    --i-feature-tables {} \
+    --o-tree demo-qemistree.qza \
+    --o-merged-feature-table {} \
+    --o-merged-feature-data {}'.format(args.conda_activate_bin, args.conda_environment, \
+        output_fingerprints_qza, \
+        output_feature_qza, \
+        output_merged_feature_table_qza, \
+        output_merged_data_qza)
+    all_cmd.append(cmd)
 
     for cmd in all_cmd:
         print(cmd)
