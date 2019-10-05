8828b99911b3e0979ae672cd0fb008f6e317b01c
Mingxun Wang <mwang87@gmail.com>
2019-10-04 17:16:04 -0700

    adding initial run of import

--- qemistree/tools/qemistree/qemistree.py
+++ qemistree/tools/qemistree/qemistree.py
@@ -1,1 +1,22 @@
-qiime tools import --input-path feature-table.biom --output-path feature-table.qza --type FeatureTable[Frequency] 
+#!/usr/bin/python
+import sys
+import getopt
+import os
+import pandas as pd
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Annotate spectra')
+    parser.add_argument("input_sirius_mgf")
+    parser.add_argument("input_quant_table")
+    parser.add_argument("output_folder")
+    parser.add_argument("qiime_bin")
+
+    args = parser.parse_args()
+
+    output_qza = os.path.join(args.output_folder, "feature-table.qza")
+    cmd = "{} tools tools import --input-path {} --output-path {} --type FeatureTable[Frequency]".format(args.qiime_bin, args.input_quant_table, output_qza)
+    os.system(cmd)
+
+if __name__ == "__main__":
+    main()
