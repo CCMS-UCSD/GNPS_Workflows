ecba0953a672a536e76478f1355882a821030888
Mingxun Wang <mwang87@gmail.com>
2019-10-04 17:33:53 -0700

    refactoring cmd execution and printing

--- qemistree/tools/qemistree/qemistree.py
+++ qemistree/tools/qemistree/qemistree.py
@@ -16,11 +16,17 @@ def main():
 
     output_feature_qza = os.path.join(args.output_folder, "feature-table.qza")
     output_mgf_qza = os.path.join(args.output_folder, "sirius.mgf.qza")
-    cmd = "LC_ALL=en_US && export LC_ALL && {} tools tools import --input-path {} --output-path {} --type FeatureTable[Frequency]".format(args.qiime_bin, args.input_quant_table, output_feature_qza)
-    os.system(cmd)
 
-    cmd = "{} tools import --input-path {} --output-path {} --type MassSpectrometryFeatures".format(args.qiime_bin, args.input_sirius_mgf, output_mgf_qza)
-    os.system(cmd)
+    all_cmd = []
+    cmd = "LC_ALL=en_US && export LC_ALL && {} tools tools import --input-path {} --output-path {} --type FeatureTable[Frequency]".format(args.qiime_bin, args.input_quant_table, output_feature_qza)
+    all_cmd.append(cmd)
+
+    cmd = "LC_ALL=en_US && export LC_ALL && {} tools import --input-path {} --output-path {} --type MassSpectrometryFeatures".format(args.qiime_bin, args.input_sirius_mgf, output_mgf_qza)
+    all_cmd.append(cmd)
+
+    for cmd in all_cmd:
+        print(cmd)
+        os.system(cmd)
 
 if __name__ == "__main__":
     main()
