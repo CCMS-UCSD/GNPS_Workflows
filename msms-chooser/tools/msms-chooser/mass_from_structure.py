48333a612409a876f980131738cbc5c989d26fbc
Mingxun Wang <mwang87@gmail.com>
2019-10-07 16:57:31 -0700

    bug fix to switch server

--- msms-chooser/tools/msms-chooser/mass_from_structure.py
+++ msms-chooser/tools/msms-chooser/mass_from_structure.py
@@ -5,7 +5,7 @@ import requests
 # this is used to query inchi, smiles using Ming's web api
 def mass_from_inchi (inchi):
     print(inchi)
-    url = "http://dorresteinappshub.ucsd.edu:5065/structuremass?inchi=%s&filtersalts"%inchi
+    url = "https://gnps-structure.ucsd.edu/structuremass?inchi=%s&filtersalts"%inchi
     response = requests.get(url)
     if response.status_code !=200 :
         print("no results")
@@ -16,7 +16,7 @@ def mass_from_inchi (inchi):
 
 def mass_from_smiles (smiles):
     print(smiles)
-    url = "http://dorresteinappshub.ucsd.edu:5065/structuremass?smiles=%s&filtersalts"%smiles
+    url = "https://gnps-structure.ucsd.edu/structuremass?smiles=%s&filtersalts"%smiles
     response = requests.get(url)
     if response.status_code !=200 :
         print("no results")
