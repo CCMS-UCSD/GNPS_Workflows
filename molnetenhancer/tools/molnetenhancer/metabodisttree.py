2cecf437500f661439aab7b9bf0961fcf698450c
Mingxun Wang <mwang87@gmail.com>
2019-10-16 15:45:28 -0700

    handling errors gracefully for metabodisttree

--- molnetenhancer/tools/molnetenhancer/metabodisttree.py
+++ molnetenhancer/tools/molnetenhancer/metabodisttree.py
@@ -21,43 +21,51 @@ print(task_information)
 classyfire_result_filename = os.path.join(args.molnetenhancer_input_folder, "ClassyFireResults_Network.txt")
 
 if task_information["workflow"] == "METABOLOMICS-SNETS-V2":
-    manifest_url = "https://gnps.ucsd.edu/ProteoSAFe/DownloadResultFile?task={}&block=main&file=qiime2_output/qiime2_manifest.tsv".format(task_id)
-    metadata_url = "https://gnps.ucsd.edu/ProteoSAFe/DownloadResultFile?task={}&block=main&file=qiime2_output/qiime2_metadata.tsv".format(task_id)
-    quantification_url = "https://gnps.ucsd.edu/ProteoSAFe/DownloadResultFile?task={}&block=main&file=cluster_buckets/".format(task_id)
 
-    manifest_filename = os.path.join(args.output_folder, "qiime2_manifest.tsv")
-    metadata_filename = os.path.join(args.output_folder, "qiime2_metadata.tsv")
-    quantification_filename = os.path.join(args.output_folder, "quantification.tsv")
-    
-    #Pull down the qiime2 data
-    with open(manifest_filename, 'wb') as f:
-        f.write(requests.get(manifest_url).content)
+    try:
+        manifest_url = "https://gnps.ucsd.edu/ProteoSAFe/DownloadResultFile?task={}&block=main&file=qiime2_output/qiime2_manifest.tsv".format(task_id)
+        metadata_url = "https://gnps.ucsd.edu/ProteoSAFe/DownloadResultFile?task={}&block=main&file=qiime2_output/qiime2_metadata.tsv".format(task_id)
+        quantification_url = "https://gnps.ucsd.edu/ProteoSAFe/DownloadResultFile?task={}&block=main&file=cluster_buckets/".format(task_id)
 
-    with open(metadata_filename, 'wb') as f:
-        f.write(requests.get(metadata_url).content)
+        manifest_filename = os.path.join(args.output_folder, "qiime2_manifest.tsv")
+        metadata_filename = os.path.join(args.output_folder, "qiime2_metadata.tsv")
+        quantification_filename = os.path.join(args.output_folder, "quantification.tsv")
+        
+        #Pull down the qiime2 data
+        with open(manifest_filename, 'wb') as f:
+            f.write(requests.get(manifest_url).content)
 
-    with open(quantification_filename, 'wb') as f:
-        f.write(requests.get(quantification_url).content)
+        with open(metadata_filename, 'wb') as f:
+            f.write(requests.get(metadata_url).content)
 
-    metabodist_endpoint = SERVER_BASE + "/processmetabodisttree"
+        with open(quantification_filename, 'wb') as f:
+            f.write(requests.get(quantification_url).content)
 
-    files = {'manifest': open(manifest_filename, 'r'), \
-        'metadata': open(metadata_filename, 'r'), \
-        'quantification': open(quantification_filename, 'r'), \
-        'classyfireresult': open(classyfire_result_filename, 'r')}
+        metabodist_endpoint = SERVER_BASE + "/processmetabodisttree"
 
-    r_post = requests.post(metabodist_endpoint, files=files, data={"type":"classical"})
-    response_dict = r_post.json()
-    
-    with open(os.path.join(args.output_folder, "metabodistree_table.qza"), 'wb') as f:
-        r = requests.get(SERVER_BASE + response_dict["table_qza"], stream=True)
-        r.raw.decode_content = True
-        shutil.copyfileobj(r.raw, f)
+        files = {'manifest': open(manifest_filename, 'r'), \
+            'metadata': open(metadata_filename, 'r'), \
+            'quantification': open(quantification_filename, 'r'), \
+            'classyfireresult': open(classyfire_result_filename, 'r')}
 
-    with open(os.path.join(args.output_folder, "metabodistree_emperor.qzv"), 'wb') as f:
-        r = requests.get(SERVER_BASE + response_dict["emperor_qzv"], stream=True)
-        r.raw.decode_content = True
-        shutil.copyfileobj(r.raw, f)
+        r_post = requests.post(metabodist_endpoint, files=files, data={"type":"classical"})
+        response_dict = r_post.json()
+        
+        with open(os.path.join(args.output_folder, "metabodistree_table.qza"), 'wb') as f:
+            r = requests.get(SERVER_BASE + response_dict["table_qza"], stream=True)
+            r.raw.decode_content = True
+            shutil.copyfileobj(r.raw, f)
+
+        with open(os.path.join(args.output_folder, "metabodistree_emperor.qzv"), 'wb') as f:
+            r = requests.get(SERVER_BASE + response_dict["emperor_qzv"], stream=True)
+            r.raw.decode_content = True
+            shutil.copyfileobj(r.raw, f)
+
+    except KeyboardInterrupt:
+        raise
+    except:
+        print("Error")
+        exit(0)
 
 if task_information["workflow"] == "FEATURE-BASED-MOLECULAR-NETWORKING":
     #Workflow versions will eventually be supported
