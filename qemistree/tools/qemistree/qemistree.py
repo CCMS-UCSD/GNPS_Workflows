19f0150e7f3151b35c0c9ecba4039c8326ab0a54
Mingxun Wang <mwang87@gmail.com>
2019-10-25 08:34:27 -0700

    adding metadata and emperor plotting

--- qemistree/tools/qemistree/qemistree.py
+++ qemistree/tools/qemistree/qemistree.py
@@ -9,6 +9,7 @@ def main():
     parser = argparse.ArgumentParser(description='Annotate spectra')
     parser.add_argument("input_sirius_mgf")
     parser.add_argument("input_quant_table")
+    parser.add_argument("input_metadata_table")
     parser.add_argument("output_folder")
     parser.add_argument("conda_activate_bin")
     parser.add_argument("conda_environment")
@@ -24,6 +25,9 @@ def main():
     output_qemistree_qza = os.path.join(args.output_folder, "qemistree.qza")
     output_merged_feature_table_qza = os.path.join(args.output_folder, "merged_feature_table.qza")
     output_merged_data_qza = os.path.join(args.output_folder, "merged_data.qza")
+    output_distance_matrix_qza = os.path.join(args.output_folder, "distance_matrix.qza")
+    output_pcoa_qza = os.path.join(args.output_folder, "pcoa.qza")
+    output_emperor_qza = os.path.join(args.output_folder, "emperor.qzv")
 
     all_cmd = []
 
@@ -75,6 +79,31 @@ def main():
         output_qemistree_qza, \
         output_merged_feature_table_qza, \
         output_merged_data_qza)
+    all_cmd.append(cmd)
+
+    cmd = 'source {} {} && LC_ALL=en_US && export LC_ALL && qiime diversity beta-phylogenetic \
+    --i-phylogeny {} \
+    --p-metric "weighted_unifrac" \
+     --o-distance-matrix {}'.format(args.conda_activate_bin, args.conda_environment, \
+        output_merged_data_qza, \
+        output_distance_matrix_qza)
+    all_cmd.append(cmd)
+
+    cmd = 'source {} {} && LC_ALL=en_US && export LC_ALL && qiime diversity pcoa \
+    --i-distance-matrix {} \
+    --o-pcoa {}'.format(args.conda_activate_bin, args.conda_environment, \
+        output_distance_matrix_qza, \
+        output_pcoa_qza)
+    all_cmd.append(cmd)
+
+    cmd = 'source {} {} && LC_ALL=en_US && export LC_ALL && qiime emperor plot \
+    --i-pcoa {} \
+    --m-metadata-file {} \
+    --p-ignore-missing-samples \
+    --o-visualization {}'.format(args.conda_activate_bin, args.conda_environment, \
+        output_pcoa_qza, \
+        args.input_metadata_table, \
+        output_emperor_qza)
     all_cmd.append(cmd)
 
     for cmd in all_cmd:
