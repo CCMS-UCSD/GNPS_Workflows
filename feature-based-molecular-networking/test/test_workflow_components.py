2a48331c42ae6fc78b20d1ccc45100c385b14f26
Mingxun Wang <mwang87@gmail.com>
2019-10-29 14:42:35 -0700

    adding tests for allegra's code

--- feature-based-molecular-networking/test/test_workflow_components.py
+++ feature-based-molecular-networking/test/test_workflow_components.py
@@ -4,5 +4,10 @@ sys.path.insert(0, "../tools/feature-based-molecular-networking/scripts/")
 
 def test_written_description():
     import write_description
-    input_xml = "reference_data/params.xml"
-    write_description.write_description(input_xml, "/dev/null")
+    input_filename = "reference_data/params.xml"
+    write_description.write_description(input_filename, "/dev/null")
+
+def test_network_stats():
+    import calculate_stats_graphml
+    input_filename = "reference_data/network.graphml"
+    calculate_stats_graphml.calculate_stats(input_filename, "/dev/null")
