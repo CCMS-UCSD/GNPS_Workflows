7ae61f8391d238e7a3a4cfcbd55ab0e80dd0c356
cmaceves <christineaceves22@gmail.com>
2019-11-05 10:59:33 -0800

    updating percentage

--- feature-based-molecular-networking/tools/feature-based-molecular-networking/scripts/metadata_parser.py
+++ feature-based-molecular-networking/tools/feature-based-molecular-networking/scripts/metadata_parser.py
@@ -6,18 +6,18 @@ from collections import Counter
 def permanova_validation(input_file):
     metadata = pd.read_csv(input_file, sep = "\t")
     columns_to_PERMANOVA = []
-
+    
     for column in metadata:
        #writing the column into a list    
         templist = metadata[column].tolist()
         number_of_labels = len(templist)
 
         #each label need to make up more than 10% of the population
-        lowest_num_labels_allowed = number_of_labels * 0.01
-
+        lowest_num_labels_allowed = number_of_labels * 0.1
+        print(lowest_num_labels_allowed)
         #counting the occurance of each label within a column 
         count_dictionary = Counter(templist)
-        
+                
         #iterating over looking to see if any category is under the limit
         for key,value in count_dictionary.items():
             #on the condition a label in ever under-represented
