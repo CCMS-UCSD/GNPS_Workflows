b2a0c3575f83d419be8875cf67ac7f6ca901fb23
lfnothias <louisfelix.nothias@gmail.com>
2020-01-17 18:17:13 -0800

    Updating reference output files

--- feature-based-molecular-networking/test/test_converters.py
+++ feature-based-molecular-networking/test/test_converters.py
@@ -56,20 +56,16 @@ class TestLoaders(unittest.TestCase):
         self.assertTrue(filecmp.cmp("./progenesis_output_IMS.mgf", "./reference_input_file_for_formatter/Progenesis/161118_pos_IMS.mgf", shallow=False))
 
     #Making sure this raises exception
-    def test_progenesis_failure(self):
-        with self.assertRaises(Exception) as context:
-            progenesis_formatter.convert_to_feature_csv("./reference_input_file_for_formatter/Progensis_MSE/missing_columns.csv", "/dev/null")
         
     def test_metaboscape(self):
-        metaboscape_formatter.convert_to_feature_csv("./reference_input_file_for_formatter/MetaboScape/Lipids.msmsonly.csv", \
-            "./metaboscape_output.csv")
-
         metaboscape_formatter.convert_to_feature_csv("./reference_input_file_for_formatter/MetaboScape/Lipids.msmsonly.csv", \
             "./metaboscape_output.csv")
 
     def test_mzmine2(self):
         mzmine2_formatter.convert_to_feature_csv("./reference_input_file_for_formatter/mzmine2/MZmine-GNPS_AG_test_featuretable.csv", \
             "./mzmine_output.csv")
+            
+        self.assertTrue(filecmp.cmp("./mzmine_output.csv", "./reference_input_file_for_formatter/mzmine2/MZmine-GNPS_AG_test_featuretable_output.csv", shallow=False))
 
     def test_openms(self):
         openms_formatter.convert_to_feature_csv("./reference_input_file_for_formatter/openms/textexporter-00000.csv", \
