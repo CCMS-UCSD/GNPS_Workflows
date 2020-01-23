a698795b55e1939878ab6cef4cfda7c8777d89ee
cmaceves <christineaceves22@gmail.com>
2020-01-22 20:47:49 -0800

    cleaning up written description getting rid of ms cluster stuff

--- search_single_spectrum/tools/search_single_spectrum/write_description.py
+++ search_single_spectrum/tools/search_single_spectrum/write_description.py
@@ -13,11 +13,6 @@ if param_obj["FILTER_PRECURSOR_WINDOW"][0] == "1":
     output_sentences.append("The data was filtered by removing all MS/MS fragment ions within +/- 17 Da of the precursor m/z.")
 if param_obj["WINDOW_FILTER"][0] == "1":
     output_sentences.append("MS/MS spectra were window filtered by choosing only the top 6 fragment ions in the +/- 50 Da window throughout the spectrum.")
-if param_obj["RUN_MSCLUSTER"] == "on":
-    output_sentences.append("The data was then clustered with MS-Cluster with a parent mass tolerance of " +
-                                                    param_obj["tolerance.PM_tolerance"][0] + " Da and a MS/MS fragment ion tolerance of " + param_obj["tolerance.Ion_tolerance"][0] +
-                                                    " Da to create consensus spectra . Further, concensus spectra that contained less than " + param_obj["CLUSTER_MIN_SIZE"][0] + " spectra" +
-                                                    " were discarded.")
 output_sentences.append("The precursor ion mass tolerance was set to %s Da and a MS/MS fragment ion tolerance of %s Da." % (param_obj["tolerance.PM_tolerance"][0], param_obj["tolerance.Ion_tolerance"][0]))
 if param_obj["FILTER_LIBRARY"][0] == "1":
     output_sentences.append("The library spectra were filtered in the same manner as the input data.")
@@ -25,8 +20,5 @@ output_sentences.append("All matches kept between input spectra and library spec
 output_sentences.append("<br><br>\n<strong>Citation</strong><br><br>\n")
 output_sentences.append('Wang, Mingxun, et al. "MASST: A Web-based Basic Mass Spectrometry Search Tool for Molecules to Search Public Data." Preprint at https://doi.org/10.1101/591016')
 output_sentences.append('<br><br>')
-
-if param_obj["RUN_MSCLUSTER"] == "on":
-    output_sentences.append('Frank, Ari M., et al. "Clustering millions of tandem mass spectra." Journal of proteome research 7.01 (2007): 113-122. PMID: 18067247, https://pubs.acs.org/doi/abs/10.1021/pr070361e')
 
 open(output_filename, "w").write(" ".join(output_sentences))
