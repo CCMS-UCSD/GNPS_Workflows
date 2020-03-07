948fbd172d7f89009f7b3c825e28e1bde20ba031
Anupriya Tripathi <anupriya.tripathi92@gmail.com>
2020-03-06 21:59:11 -0800

    fixing make hierarchy and feaature grouping for v2020.1.2

--- qemistree/tools/qemistree/qemistree.py
+++ qemistree/tools/qemistree/qemistree.py
@@ -30,16 +30,16 @@ def main():
 
     output_feature_qza = os.path.join(args.output_folder, "feature-table.qza")
     output_mgf_qza = os.path.join(args.output_folder, "sirius.mgf.qza")
-    output_fragtree_qza = os.path.join(args.output_folder, "fragmentation_trees.qza")
+    output_fragtree_qza = os.path.join(args.output_folder, "fragmentation-trees.qza")
     output_formula_qza = os.path.join(args.output_folder, "formula.qza")
     output_fingerprints_qza = os.path.join(args.output_folder, "fingerprints.qza")
     output_qemistree_qza = os.path.join(args.output_folder, "qemistree.qza")
     output_qemistree_pruned_qza = os.path.join(args.output_folder, "qemistree-pruned-smiles.qza")
     output_qemistree_grouped_table_qza = os.path.join(args.output_folder, "qemistree-grouped-table.qza")
-    output_merged_feature_table_qza = os.path.join(args.output_folder, "merged_feature_table.qza")
-    output_classified_feature_data_qza = os.path.join(args.output_folder, "classified_feature_data.qza")
-    output_merged_data_qza = os.path.join(args.output_folder, "merged_feature_data.qza")
-    output_distance_matrix_qza = os.path.join(args.output_folder, "distance_matrix.qza")
+    output_merged_feature_table_qza = os.path.join(args.output_folder, "merged-feature-table.qza")
+    output_classified_feature_data_qza = os.path.join(args.output_folder, "classified-feature-data.qza")
+    output_merged_data_qza = os.path.join(args.output_folder, "merged-feature-data.qza")
+    output_distance_matrix_qza = os.path.join(args.output_folder, "distance-matrix.qza")
     output_pcoa_qza = os.path.join(args.output_folder, "pcoa.qza")
     output_emperor_qza = os.path.join(args.output_folder, "emperor.qzv")
     output_qemistree_itol_qzv = os.path.join(args.output_folder, "qemistree-itol.qzv")
@@ -99,7 +99,7 @@ def main():
         cmd = 'source {} {} && LC_ALL=en_US.UTF-8 && export LC_ALL && qiime qemistree make-hierarchy \
         --i-csi-results {} \
         --i-feature-tables {} \
-        --i-ms2-matches {}\
+        --i-library-matches {}\
         --p-metric euclidean \
         --o-tree {} \
         --o-feature-table {} \
@@ -146,7 +146,7 @@ def main():
     --i-table {} \
     --i-phylogeny {} \
     --p-metric "weighted_unifrac" \
-     --o-distance-matrix {}'.format(args.conda_activate_bin, args.conda_environment, \
+    --o-distance-matrix {}'.format(args.conda_activate_bin, args.conda_environment, \
         output_merged_feature_table_qza, \
         output_qemistree_qza, \
         output_distance_matrix_qza)
@@ -176,6 +176,7 @@ def main():
         cmd = f'source {args.conda_activate_bin} {args.conda_environment} && LC_ALL=en_US.UTF-8 && export LC_ALL && qiime feature-table group \
         --i-table {output_feature_qza} \
         --m-metadata-column {args.sample_metadata_column} \
+        --p-axis sample \
         --m-metadata-file {metadata_files[0]} \
         --o-grouped-table {output_qemistree_grouped_table_qza} \
         --p-mode mean-ceiling'
@@ -187,7 +188,7 @@ def main():
         --i-feature-metadata {output_classified_feature_data_qza} \
         --i-grouped-table {output_qemistree_grouped_table_qza} \
         --p-category direct_parent \
-        --p-ms2-label True \
+        --p-ms2-label False \
         --p-parent-mz True \
         --o-visualization {output_qemistree_itol_qzv}'
         all_cmd.append(cmd)
