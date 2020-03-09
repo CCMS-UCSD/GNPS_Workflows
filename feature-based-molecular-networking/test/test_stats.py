756f4c10a6fb2a8e3b03dbd7ea2f5a77532671c8
Mingxun Wang <mwang87@gmail.com>
2020-03-08 22:00:38 -0700

    working to generate box plots

--- feature-based-molecular-networking/test/test_stats.py
+++ feature-based-molecular-networking/test/test_stats.py
@@ -1,6 +1,11 @@
 import sys
 sys.path.insert(0, "../tools/feature-based-molecular-networking/scripts/")
 import calculate_stats
+import os
 
 def test():
-    calculate_stats.calculate_statistics("reference_stats/feature_table.csv", "reference_stats/metadata.tsv")
+    try:
+        os.mkdir("output_plots")
+    except:
+        pass
+    calculate_stats.calculate_statistics("reference_stats/feature_table.csv", "reference_stats/metadata.tsv", output_plots_folder="output_plots")
--- feature-based-molecular-networking/tools/feature-based-molecular-networking/scripts/calculate_stats.py
+++ feature-based-molecular-networking/tools/feature-based-molecular-networking/scripts/calculate_stats.py
@@ -1,13 +1,58 @@
-#import_all_modules
+import os
 import sys
 import argparse
-import plotnine as p9
+import pandas as pd
+from plotnine import *
 
 import metadata_permanova_prioritizer
 
-def calculate_statistics(input_quant_filename, input_metadata_file, metadata_column=None):
-    if metadata_colums is None or metadata_column == "None":
-        print("Consider all columns")
+def calculate_statistics(input_quant_filename, input_metadata_file, output_plots_folder=None, metadata_column=None):
+    ## Loading feature table
+    features_df = pd.read_csv(input_quant_filename, sep=",")
+    metadata_df = pd.read_csv(input_metadata_file, sep="\t")
+
+    # removing peak area from columns
+    features_df.index = features_df["row ID"]
+    metabolite_id_list = list(features_df["row ID"])
+    headers_to_keep = [header for header in features_df.columns if "Peak area" in header]
+    features_df = features_df[headers_to_keep]
+    column_mapping = {headers:headers.replace(" Peak area", "") for headers in features_df.columns}
+    features_df = features_df.rename(columns=column_mapping)
+
+    # Transpose
+    features_df = features_df.T
+    
+    # Merging with Metadata
+    features_df["filename"] = features_df.index
+    features_df = features_df.merge(metadata_df, how="inner", on="filename")
+    
+
+    # If we do not select a column, we don't calculate stats, but we do generate nice box plots
+    if metadata_column is None or metadata_column == "None":
+        columns_to_consider = metadata_permanova_prioritizer.permanova_validation(input_metadata_file)
+        for column_to_consider in columns_to_consider:
+            print(column_to_consider)
+
+            # Loop through all metabolites, and create plots
+            if output_plots_folder is not None:
+                for metabolite_id in metabolite_id_list:
+                    long_data_df = pd.melt(features_df, id_vars=[column_to_consider], value_vars=[metabolite_id])
+                    output_filename = os.path.join(output_plots_folder, "{}_{}.png".format(column_to_consider, metabolite_id))
+
+                    p = (
+                        ggplot(long_data_df)
+                        + geom_boxplot(aes(x="factor({})".format(column_to_consider), y="value", fill=column_to_consider))
+                    )
+                    p.save(output_filename)
+
+                    print(long_data_df)
+
+                    break
+                    
+            break
+
+            
+
 
 def main():
     parser = argparse.ArgumentParser(description='Calculate some stats')
@@ -15,6 +60,7 @@ def main():
     parser.add_argument('quantification_file', help='mzmine2 style quantification filename')
     parser.add_argument('output_images_folder', help='output_images_folder')
     parser.add_argument('output_stats_folder', help='output_stats_folder')
+    parser.add_argument('--metadata_column', help='metadata_column', default=None)
     args = parser.parse_args()
 
 
