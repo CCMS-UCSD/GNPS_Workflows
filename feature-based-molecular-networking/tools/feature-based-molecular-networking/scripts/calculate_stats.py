35437c33ef3eb73fb95b92b4270785fb7c7ae352
Mingxun Wang <mwang87@gmail.com>
2020-03-08 21:08:38 -0700

    reading parameters

---
+++ feature-based-molecular-networking/tools/feature-based-molecular-networking/scripts/calculate_stats.py
@@ -0,0 +1,17 @@
+#import_all_modules
+import sys
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Calculate some stats')
+    parser.add_argument('metadata_folder', help='metadata_folder')
+    parser.add_argument('quantification_file', help='mzmine2 style quantification filename')
+    parser.add_argument('output_images_folder', help='output_images_folder')
+    parser.add_argument('output_stats_folder', help='output_stats_folder')
+    args = parser.parse_args()
+
+
+
+if __name__ == "__main__":
+    main()
