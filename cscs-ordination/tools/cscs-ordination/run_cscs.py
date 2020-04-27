00bf1eb4a73eb04467c2772af97f08a302a7b58a
Mingxun Wang <mwang87@gmail.com>
2020-04-27 11:28:24 -0700

    initial version of getting data and running cscs

---
+++ cscs-ordination/tools/cscs-ordination/run_cscs.py
@@ -0,0 +1,34 @@
+import argparse
+import pyCSCS
+import requests
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('gnps_task', help="input GNPS molecular networking task id")
+parser.add_argument('output_folder', help="output folder with results")
+
+args = parser.parse_args()
+
+# Check type of task
+status_url = "https://gnps.ucsd.edu/ProteoSAFe/status_json.jsp?task={}".format(args.gnps_task)
+status_json = requests.get(status_url).json()
+
+workflow_name = status_json["workflow"]
+
+if workflow_name == "METABOLOMICS-SNETS-V2":
+    # This is to pull down data for classical molecular networking
+    bucket_url = "https://gnps.ucsd.edu/ProteoSAFe/DownloadResultFile?task={}&file=cluster_buckets/&block=main".format(args.gnps_task)
+    print(bucket_url)
+    with open("bucket_table.tsv", "w") as outfile:
+        outfile.write(requests.get(bucket_url).text)
+
+    # This is to pull down data for classical molecular networking
+    pairs_url = "https://gnps.ucsd.edu/ProteoSAFe/DownloadResultFile?task={}&file=networkedges_selfloop/&block=main".format(args.gnps_task)
+    print(pairs_url)
+    with open("network_edges.tsv", "w") as outfile:
+        outfile.write(requests.get(pairs_url).text)
+
+    pyCSCS.cscs_from_files("bucket_table.tsv", "network_edges.tsv")
+
+    
+
+
