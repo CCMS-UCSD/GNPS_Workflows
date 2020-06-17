fd8bfdedaefcf14b8ec700c6fa20fd55d89a3264
Mingxun Wang <mwang87@gmail.com>
2020-06-17 11:41:43 -0700

    bug fixes for workflow

--- feature-based-molecular-networking/tools/feature-based-molecular-networking/scripts/filter_spectra.py
+++ feature-based-molecular-networking/tools/feature-based-molecular-networking/scripts/filter_spectra.py
@@ -2,47 +2,30 @@
 
 import sys
 import getopt
-from shutil import copyfile
-
+import argparse
 import ming_spectrum_library
 
-input_workflow_param = sys.argv[1]
-input_mgf_filename = sys.argv[2]
-output_mgf_filename = sys.argv[3]
-
-# Load the workflow params
+parser = argparse.ArgumentParser(description='Filter Spectra')
+parser.add_argument('input_mgf_filename', help='input_mgf_filename')
+parser.add_argument('output_mgf_filename', help='output_mgf_filename')
+parser.add_argument('--FILTER_PRECURSOR_WINDOW', help='0', default=None)
+parser.add_argument('--WINDOW_FILTER', help='0', default=None)
+args = parser.parse_args()
 
 # Load the spectra
-spectrum_collection = ming_spectrum_library.SpectrumCollection(input_mgf_filename)
+spectrum_collection = ming_spectrum_library.SpectrumCollection(args.input_mgf_filename)
 spectrum_collection.load_from_mgf()
 
 # Applying the Filtering
+if args.FILTER_PRECURSOR_WINDOW == "1":
+    for spectrum in spectrum_collection.spectrum_list:
+        if spectrum is not None:
+            spectrum.window_filter_peaks(50, 6)
 
+if args.FILTER_PRECURSOR_WINDOW == "1":
+    for spectrum in spectrum_collection.spectrum_list:
+        if spectrum is not None:
+            spectrum.filter_precursor_peaks()
 
 # Saving Spectra
-
-# spectrum_list = sorted(spectrum_collection.spectrum_list, key=lambda spectrum: int(spectrum.scan))
-
-# included_scans = set()
-# spectrum_dict = {}
-# for spectrum in spectrum_list:
-#     spectrum_dict[int(spectrum.scan)] = spectrum
-#     included_scans.add(int(spectrum.scan))
-
-# max_scan = max(included_scans)
-
-# output_spectrum_list = []
-# for scan_count in range(1, max_scan+1):
-#     if scan_count in spectrum_dict:
-#         output_spectrum_list.append(spectrum_dict[scan_count])
-#     else:
-#         output_spectrum_list.append(ming_spectrum_library.Spectrum(output_mgf_filename, scan_count, scan_count-1, [], 0.0, 1, 2))
-
-
-# spectrum_collection.spectrum_list = output_spectrum_list
-
-# spectrum_collection.save_to_mgf(open(output_mgf_filename, "w"))
-
-
-# print(included_scans)
-#copyfile(sys.argv[1], sys.argv[2])
+spectrum_collection.save_to_mgf(open(args.output_mgf_filename, "w"))
