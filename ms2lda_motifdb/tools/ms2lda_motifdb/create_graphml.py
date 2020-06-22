f24213609890d5b0bbd56e94d192988407d80c9b
Mingxun Wang <mwang87@gmail.com>
2020-06-22 13:51:59 -0700

    adding networking edges labelling

--- ms2lda_motifdb/tools/ms2lda_motifdb/create_graphml.py
+++ ms2lda_motifdb/tools/ms2lda_motifdb/create_graphml.py
@@ -17,7 +17,6 @@ parser.add_argument('input_network_topx', type=int, help='input_network_topx')
 args = parser.parse_args()
 
 try:
-
     # prepare data
     motifs = pd.read_csv(os.path.join(args.ms2lda_results, "output_motifs_in_scans.tsv"), sep = '\t')
     motifs = motifs.rename(columns={'scan': 'scans'})
@@ -30,23 +29,33 @@ try:
     edges = pd.read_csv(args.input_network_edges, sep="\t")
     edges = edges[["CLUSTERID1", "CLUSTERID2", "DeltaMZ", "MEH", "Cosine", "OtherScore", "ComponentIndex"]]
 
-    # run pyMolNetEnhancer
+    # Run pyMolNetEnhancer
     motif_network = Mass2Motif_2_Network(edges, motifs, prob = args.input_network_pvalue, overlap = args.input_network_overlap, top = args.input_network_topx)
     motif_network['nodes']['motifdb_url'] = motif_network['nodes']['motifdb_url'].agg(lambda x: ','.join(map(str, x)))
     motif_network['nodes']['motifdb_annotation'] = motif_network['nodes']['motifdb_annotation'].agg(lambda x: ','.join(map(str, x)))
 
-    # Consistently annotating edges with column headers consistent with IIN and Merge Network Polarity, it will be noted as EdgeType
-    # TODO
-
-    # Creating output edges
+    # Creating Output Edges
     edges_df = pd.DataFrame(motif_network['edges'])
     edges_df.to_csv(args.output_pairs, sep='\t', index=False)
 
-    # create graphml file=
+    # Create Graphml File
     MG = make_motif_graphml(motif_network['nodes'], motif_network['edges'])
-    output_graphml_filename = args.output_graphml
-    nx.write_graphml(MG, output_graphml_filename, infer_numeric_types = True)
+
+    # Consistently annotating edges with column headers consistent with IIN and Merge Network Polarity, it will be noted as EdgeType
+    #motif_network['nodes']["EdgeType"] = "EDGETYPE"
+    for edge in MG.edges(data=True):
+        edge_type = "Cosine"
+        edge_annotation = ""
+        interaction_name = MG[edge[0]][edge[1]][0]['interaction']
+        if "m2m" in interaction_name:
+            edge_type = "Mass2Motif"
+            edge_annotation = interaction_name
+        MG[edge[0]][edge[1]][0]['EdgeType'] = edge_type
+        MG[edge[0]][edge[1]][0]['EdgeAnnotation'] = edge_annotation
+
+    nx.write_graphml(MG, args.output_graphml, infer_numeric_types=True)
 except:
-    raise
     print("Error in Creating Graphml")
+    raise
+    
 
