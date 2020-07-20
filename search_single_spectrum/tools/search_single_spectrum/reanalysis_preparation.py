43a8e81fce89ce416b30237a38807584a5e2535c
Mingxun Wang <mwang87@gmail.com>
2020-07-20 13:49:55 -0700

    removing stray carriage returns

--- search_single_spectrum/tools/search_single_spectrum/reanalysis_preparation.py
+++ search_single_spectrum/tools/search_single_spectrum/reanalysis_preparation.py
@@ -21,10 +21,6 @@ def main():
         params_dict["library_on_server"] = "d.speclibs;"
         params_dict["spec_on_server"] = ";".join(all_filenames)
 
-        print(params_dict)
-
-        #reanalysis_url = "https://gnps.ucsd.edu/ProteoSAFe/index.jsp?#" + json.dumps(params_dict)
-
         output_file = open(output_filename, "w")
         output_file.write("filenames\n")
         output_file.write(";".join(all_filenames))
