b503a6ef47ea5d7ec59a4749b529e1cb3eb280bc
Robin Schmid <rschmid1789@gmail.com>
2020-11-19 20:27:00 +0100

    Added option to collapse ion networks to graphml export
    Logic in ion_network_utils.py
    - finds all ion identity nodes
    - only keeps node with maximum intensity, merges other ion into this ion node
    - redirects all edges to this node
    - removes ion identity edges
    - adds specific new attributes to collapsed "neutral molecule" nodes

--- feature-based-molecular-networking/tools/feature-based-molecular-networking/scripts/convert_networks_to_graphml.py
+++ feature-based-molecular-networking/tools/feature-based-molecular-networking/scripts/convert_networks_to_graphml.py
@@ -5,9 +5,11 @@ import sys
 import getopt
 import os
 import molecular_network_filtering_library
+import ion_network_utils
 import glob
 import networkx as nx
 import argparse
+
 
 def main():
     parser = argparse.ArgumentParser(description='Creating GraphML')
@@ -16,28 +18,47 @@ def main():
     parser.add_argument('input_librarysearch', help='input_librarysearch')
     parser.add_argument('output_graphml', help='output_graphml')
     parser.add_argument('--input_analoglibrarysearch', help='input_analoglibrarysearch')
+    # options for ion identity molecular networking:
+    # add "neutral molecule" nodes and collapse IIN edges
+    # IIN edges = supplemental pairs (input_pairsfolder)
     parser.add_argument('--input_pairsfolder', help='input_pairsfolder')
+    parser.add_argument('collapse_additional_edges', help='collapse_additional_edges')
     args = parser.parse_args()
 
-    create_graphml(args.input_pairs, args.input_clusterinfosummary, args.input_librarysearch, args.input_analoglibrarysearch, args.input_pairsfolder, args.output_graphml)
+    # boolean
+    collapse_additional_edges = (args.collapse_additional_edges is not None)
+
+    # export graphml to file
+    create_graphml(args.input_pairs, args.input_clusterinfosummary, args.input_librarysearch,
+                   args.input_analoglibrarysearch, args.input_pairsfolder, args.output_graphml,
+                   collapse_additional_edges)
 
     
-def create_graphml(input_pairs, input_clusterinfosummary, input_librarysearch, input_analoglibrarysearch, input_pairsfolder, output_graphml):
-    #Doing other filtering
+def create_graphml(input_pairs, input_clusterinfosummary, input_librarysearch, input_analoglibrarysearch,
+                   input_pairsfolder, output_graphml, collapse_additional_edges):
+    # Doing other filtering
     G = molecular_network_filtering_library.loading_network(input_pairs, hasHeaders=True)
     molecular_network_filtering_library.add_clusterinfo_summary_to_graph(G, input_clusterinfosummary)
     molecular_network_filtering_library.add_library_search_results_to_graph(G, input_librarysearch)
+    # mark all nodes as feature or ion identity nodes (ion_network_utils.NODE_TYPE_ATTRIBUTE)
+    ion_network_utils.mark_all_node_types(G)
 
+    # add analogs
+    if input_analoglibrarysearch is not None:
+        molecular_network_filtering_library.add_library_search_results_to_graph(G, input_analoglibrarysearch, annotation_prefix="Analog:")
 
+    # add additional edges - e.g. ion identity edges between different ion species of the same molecule
     if input_pairsfolder is not None:
         all_pairs_files = glob.glob(os.path.join(input_pairsfolder, "*"))
         for additional_pairs_file in all_pairs_files:
             print("Adding Additional Edges", additional_pairs_file)
             molecular_network_filtering_library.add_additional_edges(G, additional_pairs_file)
 
-    if input_analoglibrarysearch is not None:
-        molecular_network_filtering_library.add_library_search_results_to_graph(G, input_analoglibrarysearch, annotation_prefix="Analog:")
+        # collapse all ion identity networks, each into a single node
+        if collapse_additional_edges:
+            G = ion_network_utils.collapse_ion_networks(G)
 
+    # export graphml
     nx.write_graphml(G, output_graphml, infer_numeric_types=True)
 
 if __name__ == "__main__":
