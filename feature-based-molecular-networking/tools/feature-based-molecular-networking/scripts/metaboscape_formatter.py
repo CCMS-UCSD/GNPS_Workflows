c7c0cd10c75cd3ed18d7702d0f25485ecdadb91e
lfnothias <louisfelix.nothias@gmail.com>
2020-11-27 21:53:16 -1000

    Update metaboscape_formatter.py
    - Supports for MetaboScape 5.0 and retrocompatible.
    - Less rigid columns requirements.

--- feature-based-molecular-networking/tools/feature-based-molecular-networking/scripts/metaboscape_formatter.py
+++ feature-based-molecular-networking/tools/feature-based-molecular-networking/scripts/metaboscape_formatter.py
@@ -16,15 +16,17 @@ def convert_to_feature_csv(input_filename, output_filename):
     # RT - > row retention time
     # PEPMASS -> row m/z
     # filename - > filename + Peak area
+    
     try:
         input_format = pd.read_csv(input_filename,index_col=None)
     except UnicodeDecodeError:
         input_format = pd.read_csv(input_filename,index_col=None, encoding='latin1')
 
-    # Check if PASEF data files and process accordingly
+    # For MetaboScape 5.0, processing
     if 'MaxIntensity' in input_format.columns:
         intensities = input_format.loc[:, 'MaxIntensity':]
         intensities = intensities.drop(['MaxIntensity'],axis = 1)
+        intensities = intensities.drop([x for x in intensities if x.endswith('_MeanIntensity')], 1)
         metadata = input_format.loc[:,:'MaxIntensity']
         metadata_filtered = metadata[['FEATURE_ID','PEPMASS','RT']]
         results = pd.concat([metadata_filtered,intensities], axis=1)
@@ -38,12 +40,24 @@ def convert_to_feature_csv(input_filename, output_filename):
         results = results.reindex (columns=cols)
         results.to_csv(output_filename,index = False)
 
-    # If not PASEF, assuming MetaboScape files are originating from a LC-MS/MS experiment, and process accordingly
+    # If not MetaboScape 5.0, processing the older format
     else:
-        output_format = input_format.drop(['SHARED_NAME','NAME','MOLECULAR_FORMULA','ADDUCT','KEGG', 'CAS'],axis = 1)
-        output_format.columns.values[0]="row ID"
-        output_format.columns.values[1]="row retention time"
-        output_format.columns.values[2] = "row m/z"
+        #Drop fixed columns that aren't needed
+        columns_to_drop = ['SHARED_NAME','CCS','SIGMA_SCORE','NAME','NAME_METABOSCAPE','MOLECULAR_FORMULA','ADDUCT','KEGG', 'CAS']
+		output_format = input_format.drop(columns=[col for col in input_format if col in columns_to_drop], axis=1)
+		#Drop optional columns that aren't needed
+		output_format = output_format.drop([x for x in output_format if x.endswith('_MeanIntensity')], 1)
+        
+        #Isolate the intensities
+        intensities = input_format.loc[:, 'CAS':]
+        intensities = intensities.drop(['CAS'],axis = 1)
+        intensities = intensities.drop([x for x in intensities if x.endswith('_MeanIntensity')], 1)
+        #Isolate the metadata
+        metadata = input_format.loc[:,:'CAS']
+        metadata_filtered = metadata[['FEATURE_ID','PEPMASS','RT']]
+        
+        results = pd.concat([metadata_filtered,intensities], axis=1)
+        results.rename(columns={'FEATURE_ID': 'row ID','PEPMASS': 'row m/z','RT':'row retention time'}, inplace=True)
 
         for i in range(3,len(output_format.columns.values)):
             output_format.columns.values[i] += " Peak area"
