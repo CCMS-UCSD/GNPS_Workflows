f70297fab23c53f694a17abca8718ca66e296b34
Mingxun Wang <mwang87@gmail.com>
2021-02-18 16:10:33 -0800

    rewriting foodomics

---
+++ search_single_spectrum/test/test_foodomics.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+import sys
+sys.path.insert(0, "../tools/search_single_spectrum/")
+
+import foodomics_track
+
+def test():
+    input_filename = "data/foodomics/all_dataset_matchs.tsv"
+
+    matches_df = pd.read_csv(input_filename, sep="\t")
+    metadata_df = pd.read_csv("../tools/search_single_spectrum/11442_foodomics_multiproject_metadata_20200630_submission.txt", sep="\t")
+
+    foodomics_track.calculate_enrichment(matches_df, metadata_df)
+
