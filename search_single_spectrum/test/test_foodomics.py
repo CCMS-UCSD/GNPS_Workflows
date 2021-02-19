01fa52f30e0d8c11577fd66247d2938915a39bc4
Mingxun Wang <mwang87@gmail.com>
2021-02-18 16:22:04 -0800

    adding new foodomics results

--- search_single_spectrum/test/test_foodomics.py
+++ search_single_spectrum/test/test_foodomics.py
@@ -11,5 +11,7 @@ def test():
     matches_df = pd.read_csv(input_filename, sep="\t")
     metadata_df = pd.read_csv("../tools/search_single_spectrum/11442_foodomics_multiproject_metadata_20200630_submission.txt", sep="\t")
 
-    foodomics_track.calculate_enrichment(matches_df, metadata_df)
+    enrichment_df = foodomics_track.calculate_enrichment(matches_df, metadata_df)
+
+    print(enrichment_df)
 
