f6ce588ec80e4af189d9991ab519900d33b80112
lfnothias <louisfelix.nothias@gmail.com>
2021-03-31 22:41:27 -0700

    [FBMN] New reference converted files + new tests

--- feature-based-molecular-networking/test/test_converters.py
+++ feature-based-molecular-networking/test/test_converters.py
@@ -17,13 +17,15 @@ class TestLoaders(unittest.TestCase):
         msdial_formatter.convert_to_feature_csv("./reference_input_file_for_formatter/MS-DIAL/MS-DIAL-GNPS_AG_test_featuretable.txt", \
             "./msdial_output.csv")
 
-        #self.assertTrue(filecmp.cmp("./msdial_output.csv", "./reference_input_file_for_formatter/MS-DIAL/MS-DIAL-GNPS_AG_test_featuretable_reference_output_file.csv", shallow=False))
+        self.assertTrue(filecmp.cmp("./msdial_output.csv", "./reference_input_file_for_formatter/MS-DIAL/MS-DIAL-GNPS_AG_test_featuretable_reference_output_file.csv", shallow=False))
+
 
     def test_msdial_ims(self):
         msdial_formatter.convert_to_feature_csv("./reference_input_file_for_formatter/MS-DIAL/GnpsTable_1_20198191357_PASEF.txt", \
             "./msdial_ims_output.csv")
 
-        #self.assertTrue(filecmp.cmp("./msdial_ims_output.csv", "./reference_input_file_for_formatter/MS-DIAL/GnpsTable_1_20198191357_PASEF_output.csv", shallow=False))
+        self.assertTrue(filecmp.cmp("./msdial_ims_output.csv", "./reference_input_file_for_formatter/MS-DIAL/GnpsTable_1_20198191357_PASEF_output.csv", shallow=False))
+
 
     def test_progenesis(self):
         ## SONAR MSE
@@ -34,7 +36,7 @@ class TestLoaders(unittest.TestCase):
 
         progenesis_formatter.convert_mgf("./reference_input_file_for_formatter/Progenesis/SONAR_20_Yeast_MSMS.msp", "SONAR_20_Yeast_MSMS.mgf", compound_to_scan_mapping)
 
-        #self.assertTrue(filecmp.cmp("./SONAR_20_Yeast_MSMS.mgf", "./reference_input_file_for_formatter/Progenesis/SONAR_20_Yeast_MSMS.mgf", shallow=False))
+        self.assertTrue(filecmp.cmp("./SONAR_20_Yeast_MSMS.mgf", "./reference_input_file_for_formatter/Progenesis/SONAR_20_Yeast_MSMS.mgf", shallow=False))
 
         ## CATECHIN MSE
         compound_to_scan_mapping = progenesis_formatter.convert_to_feature_csv("./reference_input_file_for_formatter/Progenesis/Neg_MSE_Catechin.txt", "./Neg_MSE_Catechin_output.csv")
@@ -70,34 +72,45 @@ class TestLoaders(unittest.TestCase):
     def test_metaboscape(self):
         metaboscape_formatter.convert_to_feature_csv("./reference_input_file_for_formatter/MetaboScape/Lipids.msmsonly.csv", \
             "./metaboscape_output.csv")
+        self.assertTrue(filecmp.cmp("./metaboscape_output.csv", "./reference_input_file_for_formatter/MetaboScape/Lipids.msmsonly_converted.csv", shallow=False))
 
         metaboscape_formatter.convert_to_feature_csv("./reference_input_file_for_formatter/MetaboScape/SRM1950 Lipidomics.msmsonly.csv", \
             "./metaboscape_output2.csv")
-            
+        self.assertTrue(filecmp.cmp("./metaboscape_output2.csv", "./reference_input_file_for_formatter/MetaboScape/SRM1950 Lipidomics.msmsonly_converted.csv", shallow=False))
+
         metaboscape_formatter.convert_to_feature_csv("./reference_input_file_for_formatter/MetaboScape/metaboscape5_hops.csv", \
             "./metaboscape_output3.csv")
+        self.assertTrue(filecmp.cmp("./metaboscape_output3.csv", "./reference_input_file_for_formatter/MetaboScape/metaboscape5_hops_converted.csv", shallow=False))
+
 
     def test_mzmine2(self):
         mzmine2_formatter.convert_to_feature_csv("./reference_input_file_for_formatter/mzmine2/MZmine-GNPS_AG_test_featuretable.csv", \
             "./mzmine_output.csv")
-
         self.assertTrue(filecmp.cmp("./mzmine_output.csv", "./reference_input_file_for_formatter/mzmine2/MZmine-GNPS_AG_test_featuretable_output.csv", shallow=False))
+
 
     def test_openms(self):
         openms_formatter.convert_to_feature_csv("./reference_input_file_for_formatter/openms/textexporter-00000.csv", \
             "./openms_output.csv")
+        self.assertTrue(filecmp.cmp("./openms_output.csv", "./reference_input_file_for_formatter/openms/openms_output.csv", shallow=False))
+
 
     def test_xcms3(self):
         xcms_formatter.convert_to_feature_csv("./reference_input_file_for_formatter/xcms3/XCMS3-GNPS_AG_test_featuretable.txt", \
             "./xcms3_output.csv")
+        self.assertTrue(filecmp.cmp("./xcms3_output.csv", "./reference_input_file_for_formatter/xcms3/xcms3_output.csv", shallow=False))
+
 
     def test_xcms3_iin(self):
         xcms_formatter.convert_to_feature_csv("./reference_input_file_for_formatter/xcms3/camera_iin_quant_table_sub.txt", \
             "./xcms3_output_iin.csv")
-    
+        self.assertTrue(filecmp.cmp("./xcms3_output_iin.csv", "./reference_input_file_for_formatter/xcms3/xcms3_output_iin.csv", shallow=False))
+
+
     def test_sirius(self):
         sirius_formatter.convert_to_feature_csv("./reference_input_file_for_formatter/SIRIUS/sirius_feature_table.csv", \
-            "./sirius_output.csv")
+            "./sirius_output_converted.csv")
+        self.assertTrue(filecmp.cmp("./sirius_output_converted.csv", "./reference_input_file_for_formatter/SIRIUS/sirius_feature_table_converted.csv", shallow=False))
 
 if __name__ == '__main__':
     unittest.main()
