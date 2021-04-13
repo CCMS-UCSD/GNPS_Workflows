1e733a25c9be67faea362420732564d318ae82fd
ka-west <kiana.a.west@gmail.com>
2021-04-13 10:19:12 -0700

    foodomics test new metadata file

--- search_single_spectrum/test/test_foodomics.py
+++ search_single_spectrum/test/test_foodomics.py
@@ -9,9 +9,8 @@ def test():
     input_filename = "data/foodomics/all_dataset_matchs.tsv"
 
     matches_df = pd.read_csv(input_filename, sep="\t")
-    metadata_df = pd.read_csv("../tools/search_single_spectrum/11442_foodomics_multiproject_metadata_20200630_submission.txt", sep="\t")
+    metadata_df = pd.read_csv("../tools/search_single_spectrum/gfop_ontology_foodmasst.txt", sep="\t")
 
     enrichment_df = foodomics_track.calculate_enrichment(matches_df, metadata_df)
 
     print(enrichment_df)
-
