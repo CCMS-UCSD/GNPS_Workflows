0acc0eb37abeb7b850a53c3ceadf7f0ea159552a
ka-west <kiana.a.west@gmail.com>
2021-04-16 17:04:16 -0700

    include unit test

--- search_single_spectrum/test/test_foodomics.py
+++ search_single_spectrum/test/test_foodomics.py
@@ -10,7 +10,8 @@ def test():
 
     matches_df = pd.read_csv(input_filename, sep="\t")
     metadata_df = pd.read_csv("../tools/search_single_spectrum/gfop_ontology_foodmasst.txt", sep="\t")
+    test_output = pd.read_csv("data/foodomics/foodmasst_test_output.tsv", sep="\t")
 
     enrichment_df = foodomics_track.calculate_enrichment(matches_df, metadata_df)
 
-    print(enrichment_df)
+    pd.testing.assert_frame_equal(test_output, enrichment_df)
