955bf8593c2bf8dda98e457d0901e3c65cfbc2b4
Robin Schmid <rschmid1789@gmail.com>
2021-05-24 14:50:27 +0200

    unit tests for NIST msp and mzvault msp

--- library_conversion/test/test.py
+++ library_conversion/test/test.py
@@ -1,8 +1,20 @@
+import unittest
 import sys
 sys.path.insert(0, "../tools/library_conversion/")
 import library_conversion
 from library_conversion import InputFormat
 
-def test():
-    library_conversion.convert(InputFormat.mzvault.name, "data/IROA_small.msp", "IROA_small.mgf",
-                               "IROA_small_batch.tsv", "Robin Test PI", "Robin Test Collector")
+
+class TestLibraryConversion(unittest.TestCase):
+
+    def test_mzvault_convert_msp(self):
+        library_conversion.convert(InputFormat.mzvault.name, "data/mzvault_IROA_small.msp", "mzvault_IROA_small.mgf",
+                                   "mzvault_IROA_small_batch.tsv", "Robin Test PI", "Robin Test Collector")
+
+    def test_mona_mzmine_msp(self):
+        library_conversion.convert(InputFormat.nist_msp.name, "data/MoNA_mzmine_format.msp", "mona_mzmine.mgf",
+                                   "mona_mzmine.tsv", "Robin Test PI", "Robin Test Collector")
+
+
+if __name__ == '__main__':
+    unittest.main()
