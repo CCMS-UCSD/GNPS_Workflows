3e84fdb8cf9237bf9bc110cf8853f846b4fe3493
Robin Schmid <rschmid1789@gmail.com>
2021-07-27 20:05:53 +0200

    Added test for empty input

--- search_single_spectrum/test/test_foodomics.py
+++ search_single_spectrum/test/test_foodomics.py
@@ -37,6 +37,27 @@ class TestLoaders(unittest.TestCase):
         pd.testing.assert_frame_equal(test_output, enrichment_df, check_like=True)
 
 
+    def test_empty_matches(self):
+        input_filename = "data/foodomics/all_dataset_matchs_empty.tsv"
+        foodomics_metadata = "../tools/search_single_spectrum/gfop_ontology_foodmasst.txt"
+        foodomics_metadata_table = "../tools/search_single_spectrum/foodomics_metadata_foodmasst.tsv"
+
+        out_foodmasst = "tmp/out_foodmasst_empty.tsv"
+        out_filtered_food_metadata = "tmp/out_filtered_food_metadata_empty.tsv"
+
+        # run
+        foodomics_track.combine_food_masst(foodomics_metadata, foodomics_metadata_table, input_filename, out_foodmasst, \
+        out_filtered_food_metadata)
+
+        # should be empty file
+        self.assertTrue(os.path.isfile(out_foodmasst), "No enrichment file exported")
+        self.assertFalse(os.path.getsize(out_foodmasst) > 0, "Enrichment file is not empty - was expected for an empty "
+                                                             "input")
+
+        self.assertTrue(os.path.isfile(out_filtered_food_metadata), "No metadata table file exported")
+        self.assertFalse(os.path.getsize(out_filtered_food_metadata) > 0, "Metadata table file is not empty - was "
+                                                                          "expected for an empty input")
+
     def test_food_tree(self):
         test_output = "data/foodomics/foodmasst_test_output.tsv"
         in_html = "../tools/search_single_spectrum/interactive_tree_js/collapsible_tree_v3.html"
