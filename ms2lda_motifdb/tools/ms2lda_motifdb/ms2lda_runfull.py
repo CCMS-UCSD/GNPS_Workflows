459e83cdc5d608062d78b3097c5dd54ea680af76
Mingxun Wang <mwang87@gmail.com>
2023-05-02 09:42:36 -0700

    removing extraneous call

--- ms2lda_motifdb/tools/ms2lda_motifdb/ms2lda_runfull.py
+++ ms2lda_motifdb/tools/ms2lda_motifdb/ms2lda_runfull.py
@@ -200,7 +200,7 @@ args = parser.parse_args()
 """Grabbing the latest Motifs from MS2LDA"""
 import requests
 server_url = 'https://ms2lda.org/motifdb/'
-motifset_dict = requests.get(server_url+'list_motifsets/', verify=False).json()
+# motifset_dict = requests.get(server_url+'list_motifsets/', verify=False).json()
 # db_list = ['gnps_binned_005']  # Can update this later with multiple motif sets
 db_list = []
 
